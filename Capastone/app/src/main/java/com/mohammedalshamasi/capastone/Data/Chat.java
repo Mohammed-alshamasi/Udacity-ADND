6be094de858b595a52f07282f6b94086651a1ad2
Mohammed alshamasi <mohammed.alshamasi7@gmail.com>
2019-07-08 00:54:00 +0300

    Changes have been made from the reviewer's notes

---
+++ Capastone/app/src/main/java/com/mohammedalshamasi/capastone/Data/Chat.java
@@ -0,0 +1,50 @@
+package com.mohammedalshamasi.capastone.Data;
+
+public class Chat {
+
+    private String sender,receiver,msg;
+    private boolean isSeen;
+
+    public Chat(String sender, String receiver, String msg,Boolean isSeen) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.msg = msg;
+        this.isSeen=isSeen;
+    }
+
+    public Chat() {
+
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public boolean isSeen() {
+        return isSeen;
+    }
+
+    public void setSeen(boolean seen) {
+        isSeen = seen;
+    }
+}
